74d913f4dd8cce628c306ec335e9b02b279a73b6
faulah <rohmahfaulah@gmail.com>
2024-03-05 08:13:54 +0700

    Tugas Codelab Aquarium

---
+++ app/src/main/java/com/example/aplikasi_kotlin/aquarium.kt
@@ -0,0 +1,72 @@
+package com.example.aplikasi_kotlin
+
+fun main(args: Array<String>) {
+    println("Hello, World")
+
+    println("Hello, ${args[0]}")
+
+    val isUnit = println("This is an Expression")
+    println(isUnit)
+
+    val temperature = 10
+    val isHot = if (temperature > 50) true else false
+    println(isHot)
+
+    val message = "The water temperature is ${ if (temperature > 50) "too warm" else "OK" }."
+    println(message)
+
+    println("-- Filter --\n")
+    val decorations = listOf ("rock", "pagoda", "plastic plant", "alligator", "flowerpot")
+    println( decorations.filter {it[0] == 'p'})
+
+    println("-- Eager and Lazy Filters --")
+    // eager, creates a new list
+    val eager = decorations.filter { it [0] == 'p' }
+    println("eager: $eager")
+
+    // lazy, will wait until asked to evaluate
+    val filtered = decorations.asSequence().filter { it[0] == 'p' }
+    println("filtered: $filtered")
+
+    // force evaluation of the lazy list
+    val newList = filtered.toList()
+    println("new list: $newList")
+
+    val lazyMap = decorations.asSequence().map {
+        println("access: $it")
+        it
+    }
+
+    println("lazy: $lazyMap")
+    println("-----")
+    println("first: ${lazyMap.first()}")
+    println("-----")
+    println("all: ${lazyMap.toList()}")
+
+    val lazyMap2 = decorations.asSequence().filter {it[0] == 'p'}.map {
+        println("access: $it")
+        it
+    }
+    println("-----")
+    println("filtered: ${lazyMap2.toList()}")
+
+    val mysports = listOf("basketball", "fishing", "running")
+    val myplayers = listOf("LeBron James", "Ernest Hemingway", "Usain Bolt")
+    val mycities = listOf("Los Angeles", "Chicago", "Jamaica")
+    val mylist = listOf(mysports, myplayers, mycities)     // list of lists
+    println("-----")
+    println("Flat: ${mylist.flatten()}")
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
---
+++ app/src/main/java/com/example/aplikasi_kotlin/tugas.kt
@@ -0,0 +1,2 @@
+package com.example.aplikasi_kotlin
+
